59949319217d6cb15d8d0f890d7d1fc5b75caeb8
HASHIMOTO Masayuki <gh@mahori.mail-box.ne.jp>
2020-04-09 20:18:03 +0900

    VSCodiumに対応 (#27)

---
+++ lib/Cask/VSCodium.pm
@@ -0,0 +1,32 @@
+package Cask::VSCodium;
+
+use strict;
+use warnings;
+use utf8;
+
+use FindBin qw($RealBin);
+use lib "$RealBin/lib";
+use base qw(Cask::Base);
+
+sub new {
+  my ($class, %args) = @_;
+
+  my $self = $class->SUPER::new(version => $args{version});
+  bless $self, $class;
+
+  return $self;
+}
+
+sub url {
+  my ($self, %args) = @_;
+
+  my $version = $self->{_version};
+
+  unless ($version) {
+    return undef;
+  }
+
+  return "https://github.com/VSCodium/vscodium/releases/download/${version}/VSCodium.${version}.dmg"
+}
+
+1;
